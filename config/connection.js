24a4ba60b0ce89ba023b8db9b6d061e201fc2e1d
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-26 10:10:54 -0400

    feat: Initialize connection to database

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Import the required packages
+var mysql      = require('mysql');
+ 
+// Create database connection parameters
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'data1sK1ng$10',
+  database : 'burgers_db',
+  multipleStatements: true
+});
+ 
+// Connect to the database and display list of products
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+  }
+  // console.log('connected as id ' + connection.threadId);
+});
