78b0afbe393eb1097904ff12aa73a51de239cef5
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-25 20:56:17 -0400

    feat: Create and populate database

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+-- date: a TIMESTAMP.
+
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Creates the "burgers_db" database --
+CREATE DATABASE burgers_db;
+
+-- Makes it so all of the following code will affect bamazon --
+USE burgers_db;
+
+-- Creates the table "products" within bamazon --
+CREATE TABLE burgers
+(
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  -- Makes a string column called "burger_name" which cannot contain null --
+  burger_name VARCHAR (100) NOT NULL,
+  -- Makes a boolean column called "devoured" --
+  devoured BOOLEAN DEFAULT false,
+  -- Makes a timestamp called "date" --
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  -- Sets id as this table's primary key which means all data contained within it will be unique --
+  PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+-- Creates new rows containing data in all named columns --
+INSERT INTO burgers
+(burger_name, devoured, date)
+VALUES
+("The Crispy Bun", false, "2017-10-27 07:00:05");
+
+INSERT INTO burgers
+(burger_name, devoured, date)
+VALUES
+("Toasty Buns Burgers", false, "2017-10-27 09:10:05");   
+
+INSERT INTO burgers
+(burger_name, devoured, date)
+VALUES
+("Chickpea Burger", true, "2017-10-27 08:09:06");
