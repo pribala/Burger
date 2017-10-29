a780eb82b8d59298e443464901fed92da2fdb39c
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-29 18:48:00 -0400

    fix: Connect to the MySql database

--- config/connection.js
+++ config/connection.js
@@ -10,4 +10,12 @@ var connection = mysql.createConnection({
   multipleStatements: true
 });
  
+// Connect to the database and display list of products
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+  }
+  // console.log('connected as id ' + connection.threadId);
+});
+ 
 module.exports = connection;
