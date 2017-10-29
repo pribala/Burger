09da1ff24d1bc609fca76375eb7d3a840b8dc60a
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-29 18:59:24 -0400

    feat: Create methods that will execute the necessary MySQL commands in the controllers.

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,106 @@
-var connection = require('connection');
- 
-// Connect to the database and display list of products
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
+var connection = require("../config/connection.js");
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
   }
-  // console.log('connected as id ' + connection.threadId);
-});
 
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// Object for all our SQL statement functions.
 var orm = {
-	var selectAll = function() {
-		connection.query('SELECT item_id, product_name, price FROM products', function (error, results, fields) {
-  		// error will be an Error if one occurred during the query
-  		// results will contain the results of the query
-  		// fields will contain information about the returned results fields (if any)
-  		if(error) throw error;
-  		return results;
-	};
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-	var insertOne = function() {
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-	};
+    console.log(queryString);
 
-	var updateOne = function() {
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
 
-	};
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  
+  delete: function(table, condition, cb){
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition; 
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+  });
 };
 
+// Export the orm object for the model (cat.js).
 module.exports = orm;
