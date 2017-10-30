c53b357b6af247d17e2caacd6d07641b5605dded
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-30 17:20:23 -0400

    feat: Create handlebars for rendering views

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -49,7 +49,7 @@ router.delete("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.delete(condition, function(result) {
+  cat.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
