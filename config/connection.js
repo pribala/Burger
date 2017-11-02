e5b3bf95f836bc98b6abad7f08e89c3263798fd8
Priya Balakrishnan <andu_pri@yahoo.com>
2017-11-02 16:12:29 -0400

    feat: Add remote database connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 // Import the required packages
-var mysql      = require('mysql');
- 
+var mysql = require('mysql');
+var connection; 
 // Create database connection parameters
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'data1sK1ng$10',
-  database : 'burgers_db',
-  multipleStatements: true
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+	connection = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : 'data1sK1ng$10',
+	  database : 'burgers_db',
+	  multipleStatements: true
 });
- 
+}; 
 // Connect to the database and display list of products
 connection.connect(function(err) {
   if (err) {
